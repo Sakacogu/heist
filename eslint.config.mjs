045259e89c835d360feb8aa14d17608ffdc5f0cf
Sakacogu <sarakatrin772@gmail.com>
2025-06-14 17:26:19 +0000

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,54 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import pluginImport from "eslint-plugin-import";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
-const eslintConfig = [
+export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
 
-export default eslintConfig;
+  {
+    ignores: [".next/**/*", "node_modules/**/*", "public/**/*", "sanity/**/*"],
+  },
+
+  {
+    files: ["src/**/*.{ts,tsx,js,jsx}"],
+    plugins: { import: pluginImport },
+    rules: {
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling", "index"],
+          ],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+    },
+  },
+
+  {
+    files: [
+      "*.config.{js,cjs,mjs}",
+      "*/**/*.config.{js,cjs,mjs}",
+      "tailwind.config.{js,cjs}",
+      "postcss.config.{js,mjs}",
+    ],
+    languageOptions: { sourceType: "script" },
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-var-requires": "off",
+      "import/no-anonymous-default-export": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+    },
+  },
+];
