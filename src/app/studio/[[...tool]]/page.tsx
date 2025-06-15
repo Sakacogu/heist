bc9523945d48769109ff5992bae864edb3429a8f
Sakacogu <sarakatrin772@gmail.com>
2025-06-15 12:52:38 +0000

    Update page.tsx

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -4,6 +4,8 @@ import { Studio } from "sanity";
 
 import config from "../../../../sanity.config";
 
+export const metadata = { title: 'Heist - Studio' };
+
 export default function StudioPage() {
   return <Studio config={config} />;
 }
