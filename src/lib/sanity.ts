991c4021456d97a791b560d7f3543ae16a24581d
Sakacogu <sarakatrin772@gmail.com>
2025-06-15 19:36:27 +0000

    Update sanity.ts

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -3,6 +3,6 @@ import { createClient } from "@sanity/client";
 export const sanity = createClient({
   projectId: "vbews4kx",
   dataset: "production",
-  apiVersion: "2023-01-01",
-  useCdn: true,
+  apiVersion: "2023-01-01", // use a fixed API date for cache safety
+  useCdn: true, // fastest for read-only queries
 });
